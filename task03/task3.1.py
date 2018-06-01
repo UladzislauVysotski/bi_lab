e3b606ab39d72f09e2581f8b788720d3870a04c8
Uladzislau Vysotski <Uladzislau_Vysotski@epam.com>
2018-06-01 22:53:18 +0300

    task 7

--- task03/task3.1.py
+++
@@ -1,7 +0,0 @@
-list1 = [a + b for a in 'ab' for b in 'bcd']
-list2 = list1[::2]
-list3 = [str(i + 1) + "a" for i in range(4)]
-print(list3)
-list3.remove('2a')
-list3_copy = list3 + ['2a']
-print(list3_copy)
---
+++ task07/task7.1.py
@@ -0,0 +1,26 @@
+import re
+
+# Write a program to find all email addresses in string.
+line_in_1 = 'hfhhde Uladzislau_Vysotski@epam.com asdfv SD fdad _sdf uladzislau_vysotski@gmail.com'
+for i in re.findall(r'[\w\.-]+@[\w\.-]+', line_in_1):
+    print(i)
+print()
+
+# Write a program to find all three, four, five characters long words in a string.
+line_in_2 = 'I am in love with the craps'
+print(re.findall(r'\b\w{3,5}\b', line_in_2))
+print()
+
+# Write a program to separate and print the numbers of a given string.
+line_in_3 = 'Avasd 20 adfahdf 27'
+for i in re.findall(r'\d+', line_in_3):
+    print(i)
+print()
+
+# Write a program to replace whitespaces with an underscore and vice versa.
+line_in_4 = 'I love_to do.'
+print(line_in_4)
+line_in_4 = line_in_4.replace(" ", "_")
+print(line_in_4)
+line_in_4 = line_in_4.replace("_", " ")
+print(line_in_4)
